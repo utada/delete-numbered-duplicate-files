a0f94c46553ffc01a3a537ea68f6d6e76d3fdcad
katsuya utada <utada4@gmail.com>
2013-04-16 02:24:06 +0900

    modified:   delete-duplicate.sh 	modified:   list

--- delete-duplicate.sh
+++ delete-duplicate.sh
@@ -2,10 +2,29 @@
 
 if [ ! $1 ];then exit; fi
 
-for file in $1/*[^\)].mp3; do
-  echo "$file"
+#for file in $1/*[^\)].mp3; do
+for file in $1/*[\)].mp3; do
+  #echo "$file"
   a=$(printf '%q' "`basename "$file" .mp3`")
-  find ./$1/ -name "$a \(*\).mp3" -exec rm -v {} \;
+  #echo $a
+  find ./$1/ -name "$a \(*\).mp3" -delete
+  if grep -Fxq "$a" list
+  then
+    echo 'exists'
+    rm -v "$file"
+  else
+    echo $a
+    echo 'not exists'
+    echo $a >> list
+    mv "$file" toitunes/
+  fi
+
+done
+
+for file in $1/*.mp3; do
+  #echo "$file"
+  a=$(printf '%q' "`basename "$file" .mp3`")
+  #echo $a
   if grep -Fxq "$a" list
   then
     echo 'exists'
