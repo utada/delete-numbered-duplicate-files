bc044004277a7f0003637caa6edf44202d77b587
katsuya utada <utada4@gmail.com>
2013-08-16 03:45:54 +0900

    modified:   delete-duplicate.sh 	modified:   list

--- delete-duplicate.sh
+++ delete-duplicate.sh
@@ -4,46 +4,40 @@
 if [ ! $1 ];then exit; fi
 
 for file in $1/*.mp3; do
-  echo "$file"
+  #echo "$file"
   #a=$(printf '%q' "`basename "$file" .mp3`")
   #echo $a
   a=`basename "$file"`
   b=${a%.*}
-  c=`echo $b | sed 's/([0-9])//g' | xargs`
+  #c=`echo $b | sed 's/([0-9])//g' | xargs`
 
   if [[ $b  =~ (\([0-9]\))$ ]]
   then
-    echo $b
+    #echo $b
     rm -v "$file"
     continue
   fi
 
-  #find ./$1/ -name "$a \(*\).mp3" -delete
-  if grep -Fxq "$b" list
+  find ./$1/ -name "$a \(*\).mp3" -delete
+
+  b=`echo $b | sed 's/^ *//g'`
+  grep -Fe "$b" list
+
+  if grep -Fe "$b" list
   then
     echo 'exists'
     rm -v "$file"
   else
-    echo $b
+  #  echo $a
+  #  echo $b
     echo 'not exists'
-    echo $b >> list
+   echo $b | sed 's/^ *-* *//g' >> list
+  #  #echo $b >> list
     mv "$file" toitunes/
   fi
   echo ""
 done
 
-#for file in $1/*.mp3; do
-#  #echo "$file"
-#  a=$(printf '%q' "`basename "$file" .mp3`")
-#  #echo $a
-#  if grep -Fxq "$a" list
-#  then
-#    echo 'exists'
-#    rm -v "$file"
-#  else
-#    echo $a
-#    echo 'not exists'
-#    echo $a >> list
-#    mv "$file" toitunes/
-#  fi
-#done
+cat list | sort > list2
+mv list2 list
+
