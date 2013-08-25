cb9db0749ae7ce89717b04057605bcc8a88a8ebf
katsuya utada <utada4@gmail.com>
2013-08-25 12:10:13 +0900

    new file:   import_all.sh 	modified:   list

---
+++ import_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./delete-duplicate.sh etn
+./delete-duplicate.sh etn2
+./delete-duplicate.sh pure
+./delete-duplicate.sh pure2
+./delete-duplicate.sh pure3
+./delete-duplicate.sh sense1
+
