f4a6eedd5348a4de7365b1dcb34917d385379f63
katsuya utada <utada4@gmail.com>
2014-02-05 12:52:07 +0900

    modified:   import_all.sh 	modified:   list

--- import_all.sh
+++ import_all.sh
@@ -3,7 +3,8 @@
 ./delete-duplicate.sh etn
 ./delete-duplicate.sh etn2
 ./delete-duplicate.sh pure
-./delete-duplicate.sh pure2
+#./delete-duplicate.sh pure2
 ./delete-duplicate.sh pure3
 ./delete-duplicate.sh sense1
+./delete-duplicate.sh deepmix
 
